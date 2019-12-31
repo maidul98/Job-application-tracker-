13efb7cb045c6984daa8b6138adca75d604df896
Maidul Islam <maidul98@gmail.com>
2019-12-31 17:47:10 -0500

    added register validation

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['jwt.auth','api-header']], function () {
+Route::group(['middleware' => ['jwt.auth']], function () {
   
     // all routes to protected resources are registered here  
     Route::get('users/list', function(){
@@ -30,7 +30,6 @@ Route::group(['middleware' => ['jwt.auth','api-header']], function () {
 
 
 // Route::group, function () {
-  
 //     // The registration and login requests doesn't come with tokens 
 //     // as users at that point have not been authenticated yet
 //     // Therefore the jwtMiddleware will be exclusive of them
