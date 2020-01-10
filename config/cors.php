fd9177ea4cf72c5737c9d88c3161151907cb04e6
Maidul Islam <maidul98@gmail.com>
2020-01-10 10:28:50 -0500

    added localhost to whitelist

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,8 @@ return [
         'allow_credentials' => false,
 
         'allow_origins' => [
-            'https://cryptic-plains-58850.herokuapp.com'
+            'https://cryptic-plains-58850.herokuapp.com'.
+            'http://localhost:8080'
         ],
 
         'allow_methods' => [
